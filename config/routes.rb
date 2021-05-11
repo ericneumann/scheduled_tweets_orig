0d6fb8bd0f2077e821051fc2de8fca5a2784bb38
Eric Neumann <ene1@hey.com>
2021-05-11 09:23:51 +0800

    updated session handling

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   root to: "main#index"
   get "about-us", to: "about#index", as: "about"
+  get "sign-up", to: "registrations#new"
+  post "sign-up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
 end
