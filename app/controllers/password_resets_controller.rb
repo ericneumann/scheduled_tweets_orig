3efa502d4773f0176d0581a81c7508ea796f38fa
Eric Neumann <ene1@hey.com>
2021-05-15 11:37:29 +0800

    twitter oauth

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,7 +4,6 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-
     @user = User.find_by(email: params[:email])
 
     if @user.present?
@@ -17,15 +16,24 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
-
     @user = User.find_signed!(params[:token], purpose: "password_reset")
   rescue ActiveSupport::MessageVerifier::InvalidSignature
     redirect_to sign_in_path, alert: "Your token has expired."
   end
 
   def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Your password reset was successful"
+    else
+      render :edit
+    end
+  end
 
-    
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
   
 end
